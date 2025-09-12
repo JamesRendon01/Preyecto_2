66202cf073a80ccf54bbf28dddd06a50bf364a84
James Rendon <rendondavid328@gmail.com>
2025-09-12 07:59:32 -0500

    cambios en conexion a la base de datos

--- backend-pruebas/db/database.py
+++ backend-pruebas/db/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-MARIADB_URL = 'mysql+pymysql://root:@localhost:3306/escapade_parfaite'
+MARIADB_URL = 'mysql+pymysql://root:admin@localhost:3315/escapade_parfaite'
 
 #crear el objeto de la conexion
 engine = create_engine(MARIADB_URL)
